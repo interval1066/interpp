20adda711e8fddac4ee96b992247cc831c4434f5
Tim O'Neil <interval1066@gmail.com>
2025-02-10 15:47:40 -0800

    Started main

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+// Create properties object
+// Deserialize properties and load them into the object
+// execute command loop
+int
+main(int argc, char** argv)
+{
+	return EXIT_SUCCESS;
+}
